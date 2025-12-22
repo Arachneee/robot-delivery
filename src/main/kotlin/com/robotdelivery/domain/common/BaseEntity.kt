24b4a381cf2b708399022fc9338f96f65d5981cb
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-12-22 23:15:25 +0900

    feat: BaseEntity 추가
    - AbstractAggregateRoot를 상속하는 BaseEntity 추가
    - JPA Auditing 설정 (createdAt, updatedAt) 포함
    - pullDomainEvents() 헬퍼 메서드 제공

---
+++ src/main/kotlin/com/robotdelivery/domain/common/BaseEntity.kt
@@ -0,0 +1,30 @@
+package com.robotdelivery.domain.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.domain.AbstractAggregateRoot
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity<T : BaseEntity<T>> : AbstractAggregateRoot<T>() {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    fun pullDomainEvents(): List<Any> {
+        val events = domainEvents().toList()
+        clearDomainEvents()
+        return events
+    }
+}
